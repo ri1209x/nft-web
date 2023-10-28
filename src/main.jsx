21109fffc7a7cb02fac2aa3a030622f1872927cc
Ryuga <i.ryuga917@gmail.com>
2023-10-28 14:43:54 +0900

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import {BrowserRouter} from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
- 
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
     <Footer/>
   </React.StrictMode>,
 )
